c7411367476a1d080941ee8e3099f9052f56b279
Shashi Chauhan <your-email@example.com>
2026-07-08 13:05:34 +0530

    Update resume file and footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Footer.css";
-import resume from "../assets/Rahulcv.pdf";
+import resume from "../assets/resumes.pdf";
 const Footer = () => {
   return (
     <div className="footers" id="Footer">
